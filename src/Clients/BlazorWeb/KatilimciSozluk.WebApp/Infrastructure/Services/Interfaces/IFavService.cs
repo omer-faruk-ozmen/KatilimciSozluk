18e51a774665e953085eec4e1c62baeffd269246
Ömer Faruk Özmen <omerfarukozmen0@gmail.com>
2022-09-04 10:10:47 +0300

    Deleted BlazorWeb Project

--- src/Clients/BlazorWeb/KatilimciSozluk.WebApp/Infrastructure/Services/Interfaces/IFavService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KatilimciSozluk.WebApp.Infrastructure.Services.Interfaces;
-
-public interface IFavService
-{
-    Task CreateEntryFav(Guid entryId);
-    Task CreateEntryCommentFav(Guid entryCommentId);
-    Task DeleteEntryFav(Guid entryId);
-    Task DeleteEntryCommentFav(Guid entryCommentId);
-}
